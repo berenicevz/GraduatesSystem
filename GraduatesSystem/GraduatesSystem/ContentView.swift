4229cf9a6298d507c7878f53c68eee222bcfa90f
Berenice Villafuerte <zavala0555@gmail.com>
2020-02-17 18:04:39 -0600

    AHORA SI A TRABAJAR

---
+++ GraduatesSystem/GraduatesSystem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraduatesSystem
+//
+//  Created by berenice villafuerte on 2/17/20.
+//  Copyright © 2020 berenice. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
